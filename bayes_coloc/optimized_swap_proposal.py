52fd79960bfac3ca51d089e797af63b11fb5eac4
nies1 <thomas.nies@uni-goettingen.de>
2024-11-12 20:17:44 +0100

    slow version works on fixed parameters

--- bayes_coloc/optimized_swap_proposal.py
+++ bayes_coloc/optimized_swap_proposal.py
@@ -3,7 +3,8 @@ from math import lgamma
 from tqdm import tqdm
 from icecream import ic
 from numpy import log as ln
-from .latent_state import LatentState
+from .slow_latent_space import LatentState
+# from .latent_state import LatentState
 
 class MCMC:
     def __init__(self, x, y,
